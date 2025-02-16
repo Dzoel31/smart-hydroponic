06847cc8b15f6465cc3a220974818a054650889d
Dzoel31 <dzulfikriadjmal@gmail.com>
2025-02-16 21:04:45 +0700

    change automatic control style and logic

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -1,8 +1,8 @@
 const ws = new WebSocket("ws://172.23.0.188:10000");
 
-const pumpButton = document.getElementById("toggleButton");
+const pumpButton = document.getElementById("togglePumpButton");
 const lampButton = document.getElementById("toggleLampButton");
-const otomationButton = document.getElementById("toggleOtomation");
+const otomationButton = document.getElementById("toggleAutomation");
 
 const statusPump = document.getElementById("statusPump");
 const statusLamp = document.getElementById("statusLamp");
@@ -20,14 +20,16 @@ const moisture6 = document.getElementById("moisture6")
 const moistureAvg = document.getElementById("moistureAvg");
 const temperature_atas = document.getElementById("temperature_atas");
 const temperature_bawah = document.getElementById("temperature_bawah");
+const avg_temperature = document.getElementById("avg_temperature");
 
 
-let otomationState = 1;
+let automationState = 1;
 let pumpState = 0;
 let lampState = 0;
 
 ws.onopen = () => {
     console.log("WebSocket connection opened");
+    ws.send(JSON.stringify({ type: "pump_light_ESP8266", otomationStatus: automationState, pumpStatus: pumpState, lightStatus: lampState }));
 };
 
 ws.onmessage = (event) => {
@@ -35,6 +37,8 @@ ws.onmessage = (event) => {
     
     try {
         const data = JSON.parse(event.data);
+        pumpState = data.pumpStatus;
+        lampState = data.lightStatus;
         statusPump.textContent = `Pump Status: ${data.pumpStatus}`;
         statusLamp.textContent = `Lamp Status: ${data.lightStatus}`;
         flowRate.textContent = `${(data.flowRate == undefined) ? 0 : data.flowRate}`;
@@ -50,7 +54,10 @@ ws.onmessage = (event) => {
         moistureAvg.textContent = `${(data.moistureAvg == undefined) ? 0 : data.moistureAvg}`;
         temperature_atas.textContent = `${(data.temperature_atas == undefined)? 0 : data.temperature_atas}`;
         temperature_bawah.textContent = `${(data.temperature_bawah == undefined) ? 0 : data.temperature_bawah}`;
+        avg_temperature.textContent = `${(data.temperature_atas + data.temperature_bawah) / 2}`;
         
+        pumpButton.setAttribute("checked", pumpState);
+        lampButton.setAttribute("checked", lampState);
 
     } catch (error) {
         console.error("Error parsing JSON:", error);
@@ -61,25 +68,27 @@ ws.onclose = () => {
     console.log("WebSocket connection closed");
 };
 
-//kirim perintah otomasi
 function sendOtomationCommand() {
-    ws.send(JSON.stringify({type: "pump_light_ESP8266", otomationStatus: otomationState, pumpStatus: pumpState, lightStatus: lampState }))
+    ws.send(JSON.stringify({type: "pump_light_ESP8266", otomationStatus: automationState, pumpStatus: pumpState, lightStatus: lampState }))
 }
 
-// Kirim perintah pompa
 function sendPumpCommand() {
-    ws.send(JSON.stringify({ type: "pump_light_ESP8266", otomationStatus: otomationState, pumpStatus: pumpState, lightStatus: lampState }));
+    ws.send(JSON.stringify({ type: "pump_light_ESP8266", otomationStatus: automationState, pumpStatus: pumpState, lightStatus: lampState }));
 }
 
-// Kirim perintah lampu
 function sendLampCommand() {
-    ws.send(JSON.stringify({ type: "pump_light_ESP8266", otomationStatus: otomationState, pumpStatus: pumpState, lightStatus: lampState }));
+    ws.send(JSON.stringify({ type: "pump_light_ESP8266", otomationStatus: automationState, pumpStatus: pumpState, lightStatus: lampState }));
+}
+
+if (automationState == 1) {
+    pumpButton.disabled = true;
+    lampButton.disabled = true;
 }
 
 // Toggle otomation
-otomationButton.addEventListener("click", () => {
-    if (otomationState === 0) {
-        otomationState = 1;
+otomationButton.addEventListener('change', () => {
+    if (otomationButton.checked) {
+        automationState = 1;
         otomationButton.textContent = "Otomatis";
         
         // Jika otomasi aktif, reset pompa dan lampu ke 0
@@ -87,17 +96,12 @@ otomationButton.addEventListener("click", () => {
         lampState = 0;
         pumpButton.disabled = true;
         lampButton.disabled = true;
-        pumpButton.textContent = "Mati";
-        lampButton.textContent = "Nyala";
-        
-        sendPumpCommand(); // Kirim status pompa dan lampu setelah reset
     } else {
-        otomationState = 0;
+        automationState = 0;
         otomationButton.textContent = "Non Otomatis";
         pumpState = 0;
         lampState = 0;
-        pumpButton.textContent = "Mati";
-        lampButton.textContent = "Mati";
+
         // Jika otomasi nonaktif, tombol pompa dan lampu bisa digunakan kembali
         pumpButton.disabled = false;
         lampButton.disabled = false;
@@ -109,25 +113,21 @@ otomationButton.addEventListener("click", () => {
 
 
 // Toggle pompa
-pumpButton.addEventListener("click", () => {
-    if (pumpState === 0) {
+pumpButton.addEventListener("change", () => {
+    if (pumpButton.checked) {
         pumpState = 1;
-        pumpButton.textContent = "Nyala";
     } else {
         pumpState = 0;
-        pumpButton.textContent = "Mati";
     }
     sendPumpCommand();
 });
 
 // Toggle lampu
-lampButton.addEventListener("click", () => {
-    if (lampState === 0) {
+lampButton.addEventListener("change", () => {
+    if (lampButton.checked) {
         lampState = 1;
-        lampButton.textContent = "Nyala";
     } else {
         lampState = 0;
-        lampButton.textContent = "Mati";
     }
     sendLampCommand();
 });
