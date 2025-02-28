00b3ada4223e47b78d09717aa01d308abaa0eeb9
Dzoel31 <dzulfikriadjmal@gmail.com>
2025-02-28 09:58:53 +0700

    separate data from plant and environment

--- backend/database/iot_smart_hydroponic.sql
+++ backend/database/iot_smart_hydroponic.sql
@@ -6,16 +6,24 @@
 -- Dumped by pg_dump version 17.3
 
 SET statement_timeout = 0;
+
 SET lock_timeout = 0;
+
 SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
+
 SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
+
+SET standard_conforming_strings = ON;
+
+SET search_path TO public;
+
+SET check_function_bodies = FALSE;
+
+SET xmloption = CONTENT;
+
 SET client_min_messages = warning;
-SET row_security = off;
+
+SET row_security = OFF;
 
 SET TIMEZONE = 'Asia/Jakarta';
 
@@ -25,7 +33,7 @@ CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
 SET default_table_access_method = heap;
 --
--- Name: actuator_data; Type: TABLE; Schema: public; Owner: iot_hidroponik
+-- Name: actuator_data; Type: TABLE; Schema: public; Owner: admin_iot_hidroponik
 --
 
 DROP TABLE IF EXISTS public.actuator_data CASCADE;
@@ -36,10 +44,10 @@ CREATE TABLE IF NOT EXISTS public.actuator_data (
     "timestamp" timestamptz DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-ALTER TABLE IF EXISTS public.actuator_data OWNER TO iot_hidroponik;
+ALTER TABLE IF EXISTS public.actuator_data OWNER TO admin_iot_hidroponik;
 
 --
--- Name: sensor_data; Type: TABLE; Schema: public; Owner: iot_hidroponik
+-- Name: sensor_data; Type: TABLE; Schema: public; Owner: admin_iot_hidroponik
 --
 
 DROP TABLE IF EXISTS public.sensor_data CASCADE;
@@ -55,6 +63,18 @@ CREATE TABLE IF NOT EXISTS public.sensor_data (
     water_flowrate double precision NOT NULL,
     total_litres double precision NOT NULL,
     distancecm double precision NOT NULL,
+    "timestamp" timestamptz DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+ALTER TABLE IF EXISTS public.sensor_data OWNER TO admin_iot_hidroponik;
+
+--
+-- Name: environment_data; Type: TABLE; Schema: public; Owner: admin_iot_hidroponik
+--
+
+DROP TABLE IF EXISTS public.environment_data CASCADE;
+
+CREATE TABLE IF NOT EXISTS public.environment_data (
     temperature_atas double precision NOT NULL,
     humidity_atas integer NOT NULL,
     temperature_bawah double precision NOT NULL,
@@ -63,11 +83,19 @@ CREATE TABLE IF NOT EXISTS public.sensor_data (
     "timestamp" timestamptz DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-ALTER TABLE IF EXISTS public.sensor_data OWNER TO iot_hidroponik;
+ALTER TABLE IF EXISTS public.environment_data OWNER TO admin_iot_hidroponik;
 
-SELECT public.create_hypertable('public.sensor_data', 'timestamp', if_not_exists => TRUE);
+SELECT public.create_hypertable (
+        'public.sensor_data', 'timestamp', if_not_exists => TRUE
+    );
 
-SELECT public.create_hypertable('public.actuator_data', 'timestamp', if_not_exists => TRUE);
+SELECT public.create_hypertable (
+        'public.actuator_data', 'timestamp', if_not_exists => TRUE
+    );
+
+SELECT public.create_hypertable (
+        'public.environment_data', 'timestamp', if_not_exists => TRUE
+    );
 
 ALTER TABLE public.sensor_data
 SET (
@@ -81,9 +109,23 @@ SET (
         timescaledb.compress_segmentby = 'timestamp'
     );
 
-SELECT public.add_compression_policy('public.sensor_data', INTERVAL '1 month', if_not_exists => TRUE);
+ALTER TABLE public.environment_data
+SET (
+        timescaledb.compress,
+        timescaledb.compress_segmentby = 'timestamp'
+    );
 
-SELECT public.add_compression_policy('public.actuator_data', INTERVAL '1 month', if_not_exists => TRUE);
+SELECT public.add_compression_policy (
+        'public.sensor_data', INTERVAL '1 month', if_not_exists => TRUE
+    );
+
+SELECT public.add_compression_policy (
+        'public.actuator_data', INTERVAL '1 month', if_not_exists => TRUE
+    );
+
+SELECT public.add_compression_policy (
+        'public.environment_data', INTERVAL '1 month', if_not_exists => TRUE
+    );
 
 --
 -- PostgreSQL database dump complete
