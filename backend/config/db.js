873a32e4ca3920c715f2a198f0cc517ca25a241d
Dzoel31 <dzulfikriadjmal@gmail.com>
2025-04-13 19:24:56 +0700

    Feat: change migrations from sql file to node-pg-migrate

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-});
+const db = new Client();
 
 module.exports = db;
