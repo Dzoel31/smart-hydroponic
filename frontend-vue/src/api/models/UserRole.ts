91d7a1fd773abcf5558534ee847a96d35887fa9a
Dzulfikri Adjmal <dzulfikriadjmal@gmail.com>
2026-04-15 11:53:29 +0700

    Merge Dev-Python into Main (#22)
    Co-authored-by: semantic-release <semantic-release>
    Co-authored-by: adndrsd1 <adindarizki15@gmail.com>

---
+++ frontend-vue/src/api/models/UserRole.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export const UserRole = {
+    ADMIN: 'admin',
+    USER: 'user',
+    SUPERADMIN: 'superadmin',
+} as const;
+
+export type UserRole = (typeof UserRole)[keyof typeof UserRole];
